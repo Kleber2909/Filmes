6be07e4c2f141a8ee52881b65c2ba284879ab062
Kleber Cavalcante <kleber2909@gmail.com>
2018-12-21 15:06:04 -0300

    1

--- Home.js
+++ Home.js
@@ -11,7 +11,7 @@ export default class Home extends React.Component {
     propsApp = props;
     this.state = {email: '', senha: '' };
   }
-  
+   
   static navigationOptions = {
     title: 'Login',
   };
